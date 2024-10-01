12b01a5d7b657409441784ad4c3bc52f37b5cfa6
engabdullah-2024 <enga95311@gmail.com>
2024-10-01 14:18:01 +0300

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,19 @@
-import React, { useState } from 'react';
+// src/components/Header.js
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa'; // Import menu and close icons
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <header className="bg-gray-800 p-4 text-white">
       <nav className="container mx-auto flex justify-between items-center">
-        <Link to="/">
-          <div className="sm:text-2xl font-bold">Eng Abdullah</div>
-        </Link>
-
-        {/* Menu Icon */}
-        <div className="sm:hidden cursor-pointer" onClick={toggleMenu}>
-          {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-        </div>
-
-        {/* Menu Items */}
-        <ul className={`flex flex-col sm:flex-row space-x-0 sm:space-x-6 sm:space-y-0 space-y-4 ${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
-          <li>
-            <Link to="/about" className='hover:text-gray-300'>About</Link>
-          </li>
-          <li>
-            <Link to="/" className='hover:text-gray-300'>Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/contact" className='hover:text-gray-300'>Contact</Link>
-          </li>
+       <Link to="/"> <div className="sm:text-2xl font-bold ">Eng Abdullah</div></Link>
+        <ul className="flex space-x-6">
+        <ul className="flex space-x-6 sm:text-lg">
+          <Link to="/about"> <li className='hover:text-gray-300'>About</li></Link>
+          <Link to="/"><li className='hover:text-gray-300 '></li>Portfolio</Link> 
+          <Link to="/contact"><li className='hover:text-gray-300 '></li>Contact</Link> 
+          </ul>
+         
         </ul>
       </nav>
     </header>
